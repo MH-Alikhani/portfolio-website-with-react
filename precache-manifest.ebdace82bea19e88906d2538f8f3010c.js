2685982ecdb92a1dfa21f78ecc7f2ff76568883c
MH-Alikhani <mohammad.hosein.alikhani08@gmail.com>
2021-11-21 00:35:17 +0330

    build version

---
+++ precache-manifest.ebdace82bea19e88906d2538f8f3010c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7b3e497a2836c0b070efd66942589e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42532a98d9813692d495",
+    "url": "/static/css/main.1f107f14.chunk.css"
+  },
+  {
+    "revision": "ec1be5ba959f95d81f4c",
+    "url": "/static/js/2.2f124b65.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2f124b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42532a98d9813692d495",
+    "url": "/static/js/main.aa9eb2ea.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
